41030c53cb4c101cf4e0cfe75aeb2e369853e820
Luis Fuentes <fuentesld@gmail.com>
2023-07-09 11:10:32 -0600

    Agregamos verificacion a capura de usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import {check, validationResult } from 'express-validator'
 import Usuario from '../Model/Usuario.js'
 
 const formularioLogin = (req, res) => { 
@@ -14,8 +15,24 @@ const formularioRegistro = (req, res) => {
     }
     
     const registrar = async (req, res) => { 
+        // Validation
+        await check('nombre').notEmpty().withMessage('El nombre no puede ir vacío').run(req)
+        await check('email').isEmail().withMessage('El email no es válido').run(req)
+        await check('password').isLength({min:6}).withMessage('El password debe ser de al menos 6 caractéres').run(req)
+        await check('repetir_password').equals(req.body.password).withMessage('Los passwords no son iguales').run(req)
+                
+        let resultado = validationResult(req)
+        // if (!resultado){
+        //     return res.render(
+        //         'auth/registro'),
+        //         {errores: resultado.array()}
+        // }
+        res.json(resultado.array())
+
+
+
         const usuario = await Usuario.create(req.body)  
-        res.json(usuario)  
+        // res.json(usuario)  
     
  }
 
