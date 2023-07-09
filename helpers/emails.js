8e5887e39bb9e0c1fc20262435cc6398158563df
Luis Fuentes <fuentesld@gmail.com>
2023-07-08 20:56:02 -0600

    prototipo depurado

---
+++ helpers/emails.js
@@ -0,0 +1,36 @@
+import nodemailer from 'nodemailer'
+const emailRegistro = async (datos)=>{
+    var transport = nodemailer.createTransport(
+        {
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD,
+            }
+        }
+    )
+
+    const {email, nombre, token} = datos
+
+    // Enviar email
+    await transport.sendMail(
+        {
+            from: 'BienesRaices.com',
+            to: email,
+            subject: 'Confirma tu cuenta en BienesRaices.com',
+            text: 'Confirma tu cuenta en BienesRaices.com',
+            html: 
+                `
+                    <p>Hola ${nombre}, confirma tu cuenta en BienesRaices.com</p>
+
+                    <p>Tu cuenta ya está lista, solo debes confirmarla en el siguiente enlace </p>
+                    <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/confirmar/${token}">Confirmar Cuenta</a>
+
+                    <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
+                `
+        }
+    )
+
+}
+export {emailRegistro}
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import usuarioRoutes from './routes/usuarioRoute.js'
+import db from './config/db.js'
 
 const app = express()
+
+// DB conection
+try {
+    await db.authenticate()
+    console.log(`Conexión correcta a DB ${process.env.DB_NAME}`);
+} catch (error) {
+    console.log(error);
+}
+
 const port = 3000
 
 // Habilitar Pug
@@ -14,9 +24,10 @@ app.use(express.static('public'))
 // Routing
 app.use('/auth', usuarioRoutes)
 
+const PORT = process.env.BACKEND_PORT || 3000
 app.listen(
-    port, 
+    PORT, 
     () => {
-        console.log(`El servidor esta corriendo en el puerto ${port}`)
+        console.log(`El servidor esta corriendo en URL ${process.env.BACKEND_URL}:${PORT}`)
     }
 )
