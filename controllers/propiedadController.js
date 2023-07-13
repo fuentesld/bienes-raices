dd1efc2445457132eca573d45fa678eed9125a56
Luis Fuentes <fuentesld@gmail.com>
2023-07-13 11:18:59 -0600

    Prototipo de formulario crear propiedades

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -9,6 +9,17 @@ const admin = (req, res) => {
     )
 }
 
+const crear=(req, res) => {
+    res.render(
+        'propiedades/crear',
+        {
+            pagina:'Crear Propiedad',
+            barra:true,
+        }
+    )
+}
+
 export {
     admin,
+    crear
 }
--- routes/propiedadRoutes.js
+++ routes/propiedadRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { admin, } from '../controllers/propiedadController.js'
+import { admin, crear, } from '../controllers/propiedadController.js'
 
 const router = express.Router()
 
 router.route('/mis-propiedades')
     .get(admin)
-    .post(admin)
+
+router.route('/propiedades/crear')
+    .get(crear)    
 
 
 export default router
