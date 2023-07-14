24b8519dba22412f7392e00401306cb854d41723
Luis Fuentes <fuentesld@gmail.com>
2023-07-14 14:00:07 -0600

    agregamos pin

--- src/mapa.js
+++ src/mapa.js
@@ -2,11 +2,21 @@
     const lat = 20.67444163271174;
     const lng = -103.38739216304566;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
+    let marker;
     
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    // Pin
+    marker = new L.marker(
+        [lat, lng], 
+        {
+            draggable: true,
+            autoPan: true,
+        }
+    ).addTo(mapa)
 
 
 })()
